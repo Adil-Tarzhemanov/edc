80e33c6aa36cb31f94ad4236106e60de0b02f8f7
adiltarzhemanov <mr.tarzhemanov@mail.ru>
2023-11-04 17:26:27 +0600

    delete turkish language

--- src/i18n.js
+++ src/i18n.js
@@ -3,16 +3,20 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-    fallbackLng: 'tr',
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "ru",
     debug: true,
     detection: {
-        order: ['queryString', 'cookie'],
-        cache: ['cookie']
+      order: ["queryString", "cookie"],
+      cache: ["cookie"],
     },
     interpolation: {
-        escapevalue: false
-    }
-})
+      escapevalue: false,
+    },
+  });
 
 export default i18n;
